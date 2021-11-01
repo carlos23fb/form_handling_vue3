c6cb05e5b0994ab5f9c88525e3a6f3852f3bbc02
carlos23fb <jcflobe@gmail.com>
2021-11-01 13:05:42 -0700

    add validation logic for the inputForm component adn create computed properties

--- src/02_text_input/components/InputForm.vue
+++ src/02_text_input/components/InputForm.vue
@@ -2,13 +2,15 @@
   <div class="input-form">
     <form class="ui inverted form" @submit="submitForm">
       <div class="field">
-        <label >New Item</label>
-        <input  type="text" v-model="fields.name" placeholder="Add an Item!">
+        <label>New Item</label>
+        <input type="text" v-model="fields.name" placeholder="Add an Item!">
         <span class="error-field ">{{ fieldErrors.name }}</span>
+        <span style="float: right; color: lightgreen">  {{ fields.name.length }}/20  </span>
+        <span class="error-field" v-if="isNameInputLimitExceeded" style="display: block"> Must be under twenty characters </span>
       </div>
       <div class="ui inverted divider"></div>
       <div class="field">
-        <label >Email</label>
+        <label>Email</label>
         <input type="text" v-model="fields.email" placeholder="What's yopur email?">
         <span class="error-field ">{{ fieldErrors.email }}</span>
 
@@ -23,24 +25,27 @@
           <option>Urgent</option>
         </select>
         <span class="error-field">{{ fieldErrors.urgency }}</span>
+        <span v-if="isNotUrgent" class="error-field" style="display: block">Must be moderate to urgent</span>
       </div>
       <div class="field">
         <div class="ui inverted divider"></div>
         <div class="ui checkbox inverted">
-          <input   type="checkbox" v-model="fields.termsAndConditions">
+          <input type="checkbox" v-model="fields.termsAndConditions">
           <label>I accept the terms and conditions</label>
           <span class="error-field"> {{ fieldErrors.termsAndConditions }} </span>
         </div>
 
       </div>
 
-      <button class="ui button">Submit</button>
+      <button class="ui button" :disabled="isNotUrgent || isNameInputLimitExceeded || this.fields.urgency ===''">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 import {mapActions} from 'vuex'
+
+import isEmail from 'validator/lib/isEmail'
 
 export default {
   name: "InputForm",
@@ -52,7 +57,7 @@ export default {
         urgency: '',
         termsAndConditions: false
       },
-      fieldErrors:{
+      fieldErrors: {
         name: undefined,
         email: undefined,
         urgency: undefined,
@@ -60,15 +65,39 @@ export default {
       }
     }
   },
+  computed: {
+    isNameInputLimitExceeded(){
+      return this.fields.name.length >= 20;
+    },
+    isNotUrgent(){
+      return this.fields.urgency === 'Nonessential';
+    }
+  },
   methods: {
     ...mapActions({
       addClothingItem: 'addClothingItem'
     }),
     submitForm(evt) {
+      evt.preventDefault();
+      this.fieldErrors = this.validateForm(this.fields);
+      if (Object.keys(this.fieldErrors).length) return;
       const object_request = {...this.fields}
       this.addClothingItem(object_request)
-      evt.preventDefault();
       this.clearForm()
+    },
+    validateForm(fields) {
+      const errors = {};
+      if (!fields.name) errors.name = "New name Required";
+      if (!fields.email) errors.email = "Email Required";
+      if (!fields.urgency) errors.urgency = "Urgency Required";
+      if (!fields.termsAndConditions) errors.termsAndConditions = "Terms and conditions have to be approved";
+
+
+      if (fields.email && !isEmail(fields.email)) {
+        errors.email = 'Invalid Email'
+      }
+
+      return errors;
     },
     clearForm() {
       this.fields.name = '';
@@ -76,23 +105,17 @@ export default {
       this.fields.urgency = '';
       this.fields.termsAndConditions = false;
     }
-  },
-  validateForm(fields){
-    const errors = {};
-    if(!fields.name) errors.name = "New name Required";
-    if(!fields.email) errors.email = "Email Required";
-    if(!fields.urgency) errors.urgency = "Urgency Required";
-    if(!fields.termsAndConditions) errors.termsAndConditions = "Terms and conditions have to be approved";
-
-    return errors;
   }
 }
 </script>
 
 <style scoped>
-.input-form{
+.input-form {
   max-width: 500px;
+}
 
+.error-field {
+  color: lightcoral;
 }
 
 </style>
